8ec1bdef08e763ccb573f9055a72363613455d42
Cameron Steer <cdsteer94@gmail.com>
2013-06-17 23:07:12 +0100

    Project Start

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: cdsteer
+ * Date: 17/06/2013
+ * Time: 23:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}

37ca3a4f246f7702c5e33229e22d96a7f57f0b51
Cameron Steer <cdsteer94@gmail.com>
2013-06-17 23:11:13 +0100

    Project Start

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 /**
- * Created with IntelliJ IDEA.
- * User: cdsteer
+ * @author cdsteer
  * Date: 17/06/2013
  * Time: 23:03
- * To change this template use File | Settings | File Templates.
  */
 public class Main {
     public static void main(String args[]){
---
+++ src/Ship.java
@@ -0,0 +1,8 @@
+/**
+ * @author cdsteer
+ * date 17/06/2013
+ * time 23:08
+ */
+public class Ship {
+
+}

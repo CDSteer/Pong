8c20d108979da2e4a722780d5c99ef97c53f3d4d
Cameron Steer <cdsteer94@gmail.com>
2013-06-18 12:47:43 +0100

    Graphics Library Added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,20 @@
  * Date: 17/06/2013
  * Time: 23:03
  */
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Hello World");
+    public Main(){
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        new Main();
     }
 }
